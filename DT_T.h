e99980c7e016322f24aca529e2ca7e12bba7c3c1
Bogdan Guk <bogdanguk@gmail.com>
2019-05-25 16:15:44 +0300

    created syntax and semantic of operations with derivation tree

--- DT_T.h
+++ DT_T.h
@@ -1,38 +1,33 @@
 #pragma once
 #include "string"
+#include "list"
 
 typedef void* DT_T;
 typedef std::string Element_T;
 
 
+//PRE true
+//POST RETURN empty tree
+DT_T Create_Tree();
 
-//PRE TRUE
-//POST RETURN initialized DT_T
-DT_T Create_Derivation_Tree();
+//PRE true
+//POST  IF Tree is empty
+//		RETURN true
+//		else
+//		RETURN false
+bool Q_EmptyTree(DT_T Tree);
 
-//PRE DT_Empty(DT)
-//POST free memory for DT_T
-//NOTE: for next use of Tree we must reinitialize it by Create_Derivation_Tree()
-void Free_Derivation_Tree(DT_T Tree);
+//PRE NOT Q_Empty(Tree)
+//POST RETURN datavalue of root
+Element_T Get_Root_Data(DT_T Tree);
 
+//PRE true
+//POST IF not Q_Empty(Tree) 
+//		RETURN next subtree of Tree
+//		else
+//		RETURN empty tree
+DT_T Get_Next(DT_T Tree);
 
-//PRE TRUE
-//POST IF tree is empty
-//RETURN true
-//else
-//RETURN false
-bool DT_Empty(DT_T Tree);
-
-//PRE DT_Empty(Tree)
-//POST Tree with current element as head with value inside;
-void Add_Head(Element_T value, DT_T Tree);
-
-//PRE NOT DT_Empty(Tree)
-//POST Tree with new child at current node child list
-void Add_Child_To_Current(Element_T value, DT_T Tree)
-
-
-
-//PRE NOT DT_Empty(Tree)
-//POST head become current element
-void Go_To_Head(DT_T Tree);
+//PRE true
+//POST RETURN tree with root element value an subtrees childs
+DT_T Make_Tree(Element_T value, std::list<DT_T> childs);
